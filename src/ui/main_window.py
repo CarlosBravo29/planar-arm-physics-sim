16485d99438c6b02726bdf5d38cbbac725401a40
Carlos Bravo <110853351+CarlosBravo29@users.noreply.github.com>
2026-09-12 09:50:01 -0600

    refactor(ui): optimize dark mode and add application icon - Optimize dark mode code - Add application icon - Work on new fields and layout updates

--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -7,6 +7,7 @@ class MainWin:
         self.root = root
         self.root.title("TEST sim")
         self.root.geometry("1000x600")
+        self.root.iconbitmap("assets/icon_1.ico")
         self.create_layout()
         self.create_controls()
 
@@ -17,63 +18,104 @@ class MainWin:
         self.controls.pack(side="right", padx=50, expand=True)
 
     def create_controls(self):
+
+        self.config_fields = tk.Frame(self.controls, width=200, height=100)
+        self.config_fields.pack(side="top", expand=True, pady=10)
+        self.input_fields = tk.Frame(self.controls, width=200, height=100)
+        self.input_fields.pack(side="top", expand=True, pady=10)
+
+        ## Config Links ##
         # Link 1
-        self.link1_lbe = tk.Label(self.controls, text="L1: ")
+        self.link1_lbe = tk.Label(self.config_fields, text="L1: ")
         self.link1_lbe.grid(row=0, column=0, pady=5)
-        self.entry_1 = tk.Entry(self.controls)
+        self.entry_1 = tk.Entry(self.config_fields)
         self.entry_1.grid(row=0, column=1, pady=5)
-
         # Link 2
-        self.link2_lbe = tk.Label(self.controls, text="L2: ")
+        self.link2_lbe = tk.Label(self.config_fields, text="L2: ")
         self.link2_lbe.grid(row=1, column=0, pady=5)
-        self.entry_2 = tk.Entry(self.controls)
+        self.entry_2 = tk.Entry(self.config_fields)
         self.entry_2.grid(row=1, column=1, pady=5)
-
         # Link 3
-        self.link3_lbe = tk.Label(self.controls, text="L3: ")
+        self.link3_lbe = tk.Label(self.config_fields, text="L3: ")
         self.link3_lbe.grid(row=2, column=0, pady=5)
-        self.entry_3 = tk.Entry(self.controls)
+        self.entry_3 = tk.Entry(self.config_fields)
         self.entry_3.grid(row=2, column=1, pady=5)
+        ## END - Config Links ##
+        
+        ## Input position ##
+        # X
+        self.x_lbe = tk.Label(self.input_fields, text="X: ")
+        self.x_lbe.grid(row=0, column=0, pady=5)
+        self.x_entry_1 = tk.Entry(self.input_fields)
+        self.x_entry_1.grid(row=0, column=1, pady=5)
+        # Y
+        self.y_lbe = tk.Label(self.input_fields, text="Y: ")
+        self.y_lbe.grid(row=1, column=0, pady=5)
+        self.y_entry_2 = tk.Entry(self.input_fields)
+        self.y_entry_2.grid(row=1, column=1, pady=5)
+        # phi Angle (Orientation)
+        self.phi_lbe = tk.Label(self.input_fields, text="Orientation: ")
+        self.phi_lbe.grid(row=2, column=0, pady=5)
+        self.phi_entry_3 = tk.Entry(self.input_fields)
+        self.phi_entry_3.grid(row=2, column=1, pady=5)
+        ## END - Input position ##
 
         # Dark Mode Toggle button
-        self.dark_mode_leb = tk.Label(self.controls, text="Dark mode:")
+        self.dark_mode_leb = tk.Label(self.config_fields, text="Dark mode:")
         self.dark_mode_leb.grid(row=3, column=0, pady=15, sticky="e")
-        self.dark_mode_switch = widgets.ToggleSwitch(self.controls, width=50, height=25, command=self.on_switch_change)
+        self.dark_mode_switch = widgets.ToggleSwitch(self.config_fields, width=50, height=25, command=self.on_switch_change)
         self.dark_mode_switch.grid(row=3, column=1, pady=15, sticky="w")
 
         # Button Simulate
-        self.sim_button = tk.Button(self.controls, text="Show ans", command=self.print_ans)
+        self.sim_button = tk.Button(self.input_fields, text="Show ans", command=self.print_ans)
         self.sim_button.grid(row=4, column=0, columnspan=2, pady=15)
 
     def on_switch_change(self, state):
         if state:
-            self.link1_lbe.config(bg="#202020", fg="white")
-            self.link2_lbe.config(bg="#202020", fg="white")
-            self.link3_lbe.config(bg="#202020", fg="white")
-            self.dark_mode_leb.config(bg="#202020", fg="white")
-            self.entry_1.config(bg="#2b2b2b", fg="white")
-            self.entry_2.config(bg="#2b2b2b", fg="white")
-            self.entry_3.config(bg="#2b2b2b", fg="white")
-            self.sim_button.config(bg="#2b2b2b", fg="white")
-
-            self.root.config(bg="#202020")
-            self.controls.config(bg="#202020")
-            self.canvas.config(bg="#2b2b2b")
-            self.dark_mode_switch.config(bg="#202020")
+            bg_main = "#202020"
+            bg_entry = "#2b2b2b"
+            fg_text = "white"
         else:
-            self.link1_lbe.config(bg="white", fg="black")
-            self.link2_lbe.config(bg="white", fg="black")
-            self.link3_lbe.config(bg="white", fg="black")
-            self.dark_mode_leb.config(bg="white", fg="black")
-            self.entry_1.config(bg="white", fg="black")
-            self.entry_2.config(bg="white", fg="black")
-            self.entry_3.config(bg="white", fg="black")
-            self.sim_button.config(bg="white", fg="black")
+            bg_main = "white"
+            bg_entry = "white"
+            fg_text = "black"
+        
+        labels = [
+            self.link1_lbe,
+            self.link2_lbe,
+            self.link3_lbe,
+            self.x_lbe,
+            self.y_lbe,
+            self.phi_lbe,
+            self.dark_mode_leb
+        ]
 
-            self.root.config(bg="white")
-            self.controls.config(bg="white")
-            self.canvas.config(bg="white")
-            self.dark_mode_switch.config(bg="white")
+        entries = [
+            self.entry_1,
+            self.entry_2,
+            self.entry_3,
+            self.x_entry_1,
+            self.y_entry_2,
+            self.phi_entry_3
+        ]
+
+        frames = [
+            self.root,
+            self.controls,
+            self.config_fields,
+            self.input_fields
+        ]
+
+        for label in labels:
+            label.config(bg=bg_main, fg=fg_text)
+        for entry in entries:
+            entry.config(bg=bg_entry, fg=fg_text)
+        for frame in frames:
+            frame.config(bg=bg_main)
+
+        self.canvas.config(bg=bg_entry)
+        self.sim_button.config(bg=bg_entry, fg=fg_text)
+        self.dark_mode_switch.config(bg=bg_main)
 
     def print_ans(self):
         l1 = self.entry_1.get()
